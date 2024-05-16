91383cbc9131531055218ab94195622f94586812
“yjw” <3439395450@qq.com>
2024-05-16 15:55:34 +0800

    项目创建完成

---
+++ src/main/java/com/hito/HitOApplication.java
@@ -0,0 +1,13 @@
+package com.hito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HitOApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hito/HitOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitOApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
